546d7827d1c1597e14bde57cf4fd4b27aac066ea
klurgitu <klurgitu@uncg.edu>
2020-11-17 13:54:57 -0500

    Meter names and labels info

--- app.py
+++ app.py
@@ -4,8 +4,8 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.express as px
 import pandas as pd
+import plotly.express as px
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
